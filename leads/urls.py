5d2b6849966a4eca6462e33e793db4b2763f5cef
Petya123 <alekseenkopetr25@gmail.com>
2025-01-27 18:18:54 +0500

    started categories

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView, AssignAgentView
+from .views import LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView, AssignAgentView, CategoryListView
 
 app_name = 'leads'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name = 'lead_delete'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name = 'lead_update'),
     path('create/', LeadCreateView.as_view(), name = 'lead_create'),
-    path('<int:pk>/assign_agent/', AssignAgentView.as_view(), name='assign_agent')
+    path('<int:pk>/assign_agent/', AssignAgentView.as_view(), name='assign_agent'),
+    path('categories/', CategoryListView.as_view(), name = 'category_list')
 ]
--- leads/views.py
+++ leads/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from agents.mixins import OrganisorAndLoginRequiredMixin
-from .models import Lead
+from .models import Lead, Agent, Category
 from .forms import LeadModelForm, CustomUserCreationForm, AssignAgentForm
 # Create your views here.
 
@@ -123,6 +123,32 @@ class AssignAgentView(OrganisorAndLoginRequiredMixin, generic.FormView):
         lead.agent = agent
         lead.save()
         return super(AssignAgentView, self).form_valid(form)
+
+class CategoryListView(LoginRequiredMixin, generic.ListView):
+    template_name = "leads/category_list.html"
+    context_object_name = "category_list"
+    
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_organisor:
+            queryset = Lead.objects.filter(organisation = user.userprofile)
+        else:
+            queryset = Lead.objects.filter(organisation = user.agent.organisation)
+            
+        context.update({
+            "unassigned_lead_count": queryset.filter(category__isnull = True).count()
+        })
+        print(queryset.filter(category__isnull = True))
+        return context
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_organisor:
+            queryset = Category.objects.filter(organisation = user.userprofile)
+        else:
+            queryset = Category.objects.filter(organisation = user.agent.organisation)
+        return queryset
 
 def logout_view(request):
     logout(request)
